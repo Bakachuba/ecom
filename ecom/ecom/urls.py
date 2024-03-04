f473d94f6d2cf469e54a05b05768e8d953a7d622
Backachuba <phaktonlol@gmail.com>
2024-03-05 02:33:19 +0600

    cart added

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -20,7 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('store.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', include('store.urls')),
+                  path('cart/', include('cart.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
